2a89960ff3fe3c3226077c40de91a0873b2770e3
mgz693 <megazone693@corp.auction.co.kr>
2015-11-13 10:09:44 +0900

    Fix leaveAll fail bug.

--- qufox-client.js
+++ qufox-client.js
@@ -1,8 +1,8 @@
 (function () {
 	"use strict";
-	
-	this.Qufox = function (url) { return new QufoxClient(url || "http://qufox.com"); }
-	
+
+	this.Qufox = function (url) { return new QufoxClient(url || "http://qufox.com"); };
+
 	var QufoxClient = (function () {
 		function QufoxClient(url) {
 			var self = this;
@@ -34,7 +34,7 @@
 			this.socket.on('reconnect_failed', function () { self.setStatus('reconnect_failed'); });
 			this.socket.on('reconnect', function () { self.setStatus('reconnect'); self.reconnectFlag = true; });
 			this.socket.on('reconnecting', function () { self.setStatus('reconnecting'); });
-			
+
 			this.socket.on('receive', function (payload) {
 				if (payload && payload.id) {
 					var callbackArray = self.sessionCallbackMap[payload.id];
@@ -45,31 +45,31 @@
 					}
 				}
 			});
-			
+
 			this.setStatus = function (status) {
 				if (self.status !== status) {
 					self.status = status;
 					for (var i = 0; i < self.statusChangedCallbackArray.length; ++i) {
 						self.statusChangedCallbackArray[i](status);
 					}
-					
+
 					if (self.socket.connected && (status === 'connected' || status === 'reconnected')) {
 						self.reJoin();
 					}
 				}
 			};
-			
+
 			this.reJoin = function () {
 				for (var sessionId in self.sessionCallbackMap) {
 					self.socketClient.join(sessionId, function () { });
 				}
 			};
 		}
-		
+
 		QufoxClient.prototype.onStatusChanged = function (callback) {
 			if (isFunction(callback)) this.statusChangedCallbackArray.push(callback);
-		}
-		
+		};
+
 		QufoxClient.prototype.subscribe =
 		QufoxClient.prototype.on =
 		QufoxClient.prototype.join = function (sessionId, callback) {
@@ -85,7 +85,7 @@
 				self.sessionCallbackMap[sessionId].push(callback);
 			}
 		};
-		
+
 		QufoxClient.prototype.publish =
 		QufoxClient.prototype.send = function () { // sessionId, data, [echo], callback
 			if (arguments.length == 3) // non echo
@@ -95,22 +95,22 @@
 			else
 				throw 'Argument exception.';
 		};
-		
+
 		QufoxClient.prototype.unsubscribe =
 		QufoxClient.prototype.off =
 		QufoxClient.prototype.leave = function (sessionId, callback) {
 			var self = this;
 			var currentMap = self.sessionCallbackMap[sessionId];
 			if (!currentMap) return;
-			
+
 			var excludeMap = [];
 			if (callback) {
 				for (var i = 0; i < currentMap.length; ++i) {
 					if (currentMap[i] != callback) excludeMap.push(currentMap[i]);
 				}
 			}
-			
-			if (excludeMap.length == 0) {
+
+			if (excludeMap.length === 0) {
 				self.socketClient.leave(sessionId, function (data) {
 					delete self.sessionCallbackMap[sessionId];
 				});
@@ -119,34 +119,34 @@
 				self.sessionCallbackMap[sessionId] = excludeMap;
 			}
 		};
-		
+
+		QufoxClient.prototype.unsubscribeAll =
+		QufoxClient.prototype.offAll =
 		QufoxClient.prototype.leaveAll = function () {
 			var self = this;
 			for (var sessionId in self.sessionCallbackMap) {
-				self.socketClient.leave(sessionId, function () {
-					delete self.sessionCallbackMap[sessionId];
-				});
+				self.leave(sessionId);
 			}
 		};
-		
+
 		return QufoxClient;
 	})();
-	
+
 	var SocketClient = (function () {
 		function SocketClient(socket) {
 			var self = this;
 			this.callbackMap = {};
 			this.socket = socket;
-			
+
 			self.socket.on('callback', function (response) {
 				var callback = self.callbackMap[response.id];
 				if (callback) {
 					delete self.callbackMap[response.id];
 					callback(response.data);
-				};
+				}
 			});
-		};
-		
+		}
+
 		SocketClient.prototype.join = function (sessionId, callback) {
 			var payloadId = randomString(8);
 			if (isFunction(callback)) {
@@ -154,7 +154,7 @@
 			}
 			this.socket.emit('join', { id: payloadId, sessionId: sessionId });
 		};
-		
+
 		SocketClient.prototype.send = function (sessionId, data, echo, callback) {
 			var payloadId = randomString(8);
 			var payload = { id: payloadId, sessionId: sessionId, data: data, echo: echo };
@@ -163,7 +163,7 @@
 			}
 			this.socket.emit('send', payload);
 		};
-		
+
 		SocketClient.prototype.leave = function (sessionId, callback) {
 			var payloadId = randomString(8);
 			if (isFunction(callback)) {
@@ -171,30 +171,29 @@
 			}
 			this.socket.emit('leave', { id: payloadId, sessionId: sessionId });
 		};
-		
+
 		return SocketClient;
 	})();
-	
+
 	//---- tool
 	function randomString(length) {
 		var letters = 'abcdefghijklmnopqrstuvwxyz';
 		var numbers = '1234567890';
 		var charset = letters + letters.toUpperCase() + numbers;
-		
+
 		function randomElement(array) {
 			return array[Math.floor(Math.random() * array.length)];
 		}
-		
+
 		var result = '';
 		for (var i = 0; i < length; i++)
 			result += randomElement(charset);
 		return result;
 	}
-	
+
 	function isFunction(object) {
 		return !!(object && object.constructor && object.call && object.apply);
 	}
 
 
 }.call(this));
-
